c9f2942c7aa15d130ab52e4d3b1afcd36a5bb1d7
Wes Baker <wes@wesbaker.com>
2019-12-01 20:42:35 -0500

    Add /uses page

--- src/components/bio.js
+++ src/components/bio.js
@@ -55,7 +55,8 @@ const Bio = () => {
         puzzles, and making things work. When I’m not in front of a screen of
         some sort, I’m probably spending time with my wife, my son, my animals,
         my board games, or my books.{" "}
-        <Link to="now">Check out what I'm up to now</Link>.
+        <Link to="now">Check out what I'm up to now</Link> or{" "}
+        <Link to="uses">see what tools I'm currently using</Link>.
       </p>
     </div>
   );
--- src/pages/uses.js
+++ src/pages/uses.js
@@ -14,7 +14,58 @@ class Uses extends React.Component {
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="Uses" />
         <h1>Uses</h1>
-        <p>Sometimes I use things.</p>
+        <p>
+          I get{" "}
+          <a href="https://wesbos.com/uses/">no emails about what I use</a>, but
+          I love bikeshedding about tools. Here's what I'm currently using.
+        </p>
+        <h2>Editor & Terminal</h2>
+        <ul>
+          <li>
+            <a href="https://code.visualstudio.com">Visual Studio Code</a> is my
+            editor. A younger me would be horrified that I was both using and
+            enjoying a Microsoft product.
+          </li>
+          <li>
+            I mostly use Wes Bos's{" "}
+            <a href="https://marketplace.visualstudio.com/items?itemName=wesbos.theme-cobalt2">
+              Cobalt2
+            </a>{" "}
+            themes everywhere.
+          </li>
+          <li>
+            I use{" "}
+            <a href="https://www.typography.com/fonts/operator/overview">
+              Operator Mono
+            </a>{" "}
+            for my editor font everywhere.
+          </li>
+          <li>
+            I use <a href="https://iterm2.com">iTerm</a> for my terminal. I
+            tried using <a href="https://hyper.is">Hyper</a>, but had a number
+            of performance issues that are likely unique to anyone using a
+            terminal where there are thousands of lines being added every
+            minute.
+          </li>
+        </ul>
+        <h2>Desktop Apps</h2>
+        <ul>
+          <li>
+            I live by <a href="https://www.alfredapp.com">Alfred</a> and have
+            made{" "}
+            <a href="https://github.com/wesbaker?utf8=✓&tab=repositories&q=alfred&type=&language=">
+              a number of workflows
+            </a>{" "}
+            for it.
+          </li>
+          <li>
+            I use <a href="https://todoist.com/">Todoist</a> until Things
+            supports teams/families.
+          </li>
+          <li>
+            All of my notes go into <a href="https://bear.app">Bear</a>.
+          </li>
+        </ul>
         <hr style={{ marginBottom: "1.45rem" }} />
         <Bio />
       </Layout>
